4f7eb73810c4e4b63b7d73b887bc3c0df7c81624
Grace Lee <eunheh@gmail.com>
2016-10-14 16:35:37 -0400

    friday work

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 //total number of items in data = Results number
-var resultCount = data.count
-$(".resultCount").html(resultCount);
+var numberResult = data.count
+$(".resultCount").html(`(${numberResult} Results)`);
 
 var title = data.results[0].title;
 var shop = data.results[0].Shop.shop_name;
 var price = data.results[0].price;
 var url = data.results[0].url;
-
-//not sure how to test image
 var picture = data.results[0].Images[0].url_170x135;
 
 //NEEDED ON EACH BOARD
@@ -17,13 +15,13 @@ var picture = data.results[0].Images[0].url_170x135;
 
 // //it's a 4 by 4 = 16 squares
 
-
 function renderBox (item) {
   return ` <div class ="item">
   <img class="picture" src="${item.Images[0].url_170x135}">
   <div class='title'> ${item.title}</div>
   <div class="shop"> ${item.Shop.shop_name}</div>
-  <div class="price"> ${item.price}</div>
+  <div class="price"> $${item.price}</div>
+  <a class="link" href="${item.url}">
   </div>
   `;
 };
@@ -31,7 +29,7 @@ function renderBox (item) {
 var spacesHTML = "";
 for (var i = 0; i < data.results.length; i++) {
   var newHTML = renderBox(data.results[i]);
-spacesHTML = spacesHTML + newHTML;
+  spacesHTML = spacesHTML + newHTML;
 }
 
 $(".items").html(spacesHTML);
